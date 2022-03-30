9c752868678a2e8236e8cbc73e049559d5c450f2
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-03-30 18:27:34 +0200

    WDP-9 Complete the about page

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import About from "../pages/About";
 
 function App() {
     return (
         <div className="app">
             <Header />
             <Main />
+            <About />
         </div>
     );
 }
