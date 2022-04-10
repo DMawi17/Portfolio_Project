ef5dc09e83394fd63f434e772b82fdd802b24900
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-04-10 17:14:09 +0200

    WDP-54 Clean up

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -2,20 +2,14 @@ import Header from "../layout/Header";
 import Main from "../layout/Main";
 import Footer from "../layout/Footer";
 import { ScrollUpBtn } from "../components/Buttons";
-// import Service from "../components/service/Service";
-// import { useTheme } from "../contexts/theme";
 
 function App() {
-    // const { theme, isDark } = useTheme();
-    // console.log(isDark);
-
     return (
-        <div className="app">
+        <div className="dark">
             <Header />
             <Main />
             <Footer />
             <ScrollUpBtn />
-            {/* <Service /> */}
         </div>
     );
 }
