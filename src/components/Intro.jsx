e97dc15aaea4409566cfca21506a03b0fc140df2
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-04-10 12:11:44 +0200

    WDP-6 Add animation on the intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,6 @@
 import { MainBtn } from "./Buttons";
 import { useData } from "../contexts/data-hooks";
+import { motion } from "framer-motion";
 
 function Intro() {
     const { intro, buttons } = useData();
@@ -9,9 +10,31 @@ function Intro() {
 
     return (
         <div className="intro">
-            <h1 className="intro__title">{intro_title.title}</h1>
-            <h3 className="intro__subtitle">{intro_title.subtitle}</h3>
-            <p className="intro__description">{intro_description}</p>
+            <motion.h1
+                initial={{ x: "-100vw" }}
+                animate={{ x: 0 }}
+                transition={{ type: "spring", duration: 1.5, bounce: 0.3 }}
+                className="intro__title"
+            >
+                {intro_title.title}
+            </motion.h1>
+
+            <motion.h3
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1.5 }}
+                className="intro__subtitle"
+            >
+                {intro_title.subtitle}
+            </motion.h3>
+            <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2 }}
+                className="intro__description"
+            >
+                {intro_description}
+            </motion.p>
 
             <a href="#contact" className="button button-flex intro__button">
                 <MainBtn {...introBtn} />
