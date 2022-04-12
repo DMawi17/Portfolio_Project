d9eea7a6af49ba361e02151076420344e839e920
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-04-12 02:56:17 +0200

    --amend

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,17 +1,16 @@
-// import { Routes, Route } from "react-router-dom";
-// import Whoops404 from "../pages/Whoops404";
-// import ThankYou from "../pages/ThankYou";
+import { Routes, Route } from "react-router-dom";
+import Whoops404 from "../components/contact/Whoops404";
+import ThankYou from "../components/contact/ThankYou";
 import Home from "./Home";
 
 function App() {
     return (
         <div className="app">
-            {/* <Routes> */}
-            {/* <Route exact path="/" element={<Home />} /> */}
-            {/* <Route path="/ThankYou" element={<ThankYou />} /> */}
-            {/* <Route  path="*" element={<Whoops404 />} /> */}
-            {/* </Routes> */}
-            <Home />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/ThankYou" element={<ThankYou />} />
+                <Route path="*" element={<Whoops404 />} />
+            </Routes>
         </div>
     );
 }
